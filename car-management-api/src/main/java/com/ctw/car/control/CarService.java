a97920e6a36392d9ff6c8af2b48737cc92c7ddef
Ruben Correia <Ruben.Correia@ctw.bmwgroup.com>
2024-09-26 22:44:22 +0100

    Code cleanup

--- car-management-api/src/main/java/com/ctw/car/control/CarService.java
+++ car-management-api/src/main/java/com/ctw/car/control/CarService.java
@@ -2,48 +2,22 @@ package com.ctw.car.control;
 
 import com.ctw.car.entity.Car;
 import com.ctw.car.entity.EngineType;
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
-@ApplicationScoped
+@Dependent
 public class CarService {
-    private final Map<UUID, Car> cars = new HashMap<>();
+    private final CarRepository carRepository;
 
-    private CarService() {
-        // Populate initially to have some values, TO BE REMOVED
-        UUID id = UUID.randomUUID();
-        cars.put(id, new Car(id, "BMW", "3-series", EngineType.PHEV));
-        id = UUID.randomUUID();
-        cars.put(id, new Car(id, "BMW", "4-series", EngineType.DIESEL));
-        id = UUID.randomUUID();
-        cars.put(id, new Car(id, "Rolls-Royce", "Spectre", EngineType.BEV));
+    @Inject
+    public CarService(final CarRepository carRepository) {
+        this.carRepository = carRepository;
     }
 
     public List<Car> getCars() {
-        return cars.values().stream().toList();
-    }
-
-    public Car createCar(Car car) {
-        car.setId(UUID.randomUUID());
-        cars.put(car.getId(), car);
-        return car;
-    }
-
-    public void deleteCar(UUID id) {
-        cars.remove(id);
-    }
-
-    public Car getCar(UUID id) {
-        return cars.get(id);
-    }
-
-    public Car updateCar(UUID id, Car car) {
-        Car result = null;
-        if (Objects.nonNull(car) && Objects.nonNull(id) && cars.containsKey(id)) {
-            result = cars.put(id, car);
-        }
-
-        return result;
+        return carRepository.fetchAllCars();
     }
 }
