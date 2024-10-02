62950a2a8cd36ae8d2360676afff54a954a5c6f3
DS3000 <davidsa3000@gmail.com>
2024-10-02 15:10:30 +0100

    Changed T_RESERVATION table. Added populate reservation table script.

---
+++ car-management-api/db/changelog/master/1.0.0/sql/05-populate-reservation-table.sql
@@ -0,0 +1,7 @@
+INSERT INTO car_management.t_reservation(reserve_name,contact,license,pick_up,drop_off,car_id) VALUES('David',  '962987654','987654321','2024-10-04 14:14:36','2024-10-07 08:00:00','c9305ad0-04f2-4678-8d95-99c10d747dc7');
+INSERT INTO car_management.t_reservation(reserve_name,contact,license,pick_up,drop_off,car_id) VALUES('Carlos', '910789456','987654321','2024-10-04 14:14:36','2024-10-07 08:00:00','83403494-cf23-437f-a095-dd723e81ff93');
+INSERT INTO car_management.t_reservation(reserve_name,contact,license,pick_up,drop_off,car_id) VALUES('André',  '927894567','987654321','2024-10-04 14:14:36','2024-10-07 08:00:00','6fbbd079-e94e-442a-ac49-3fecf399612f');
+INSERT INTO car_management.t_reservation(reserve_name,contact,license,pick_up,drop_off,car_id) VALUES('Miguel', '981111110','987654321','2024-10-04 14:14:36','2024-10-07 08:00:00','8e1a4f21-00e0-4778-80ab-d5cedf4dd748');
+INSERT INTO car_management.t_reservation(reserve_name,contact,license,pick_up,drop_off,car_id) VALUES('Ricardo','915555555','987654321','2024-10-04 14:14:36','2024-10-07 08:00:00','dc44b697-bb0f-4abb-bfa0-6dae6e16bd0c');
+INSERT INTO car_management.t_reservation(reserve_name,contact,license,pick_up,drop_off,car_id) VALUES('Pedro',  '967777777','987654321','2024-10-04 14:14:36','2024-10-07 08:00:00','cbaad6ce-17e2-4134-8144-c6e9307f7e77');
+
